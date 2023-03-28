00d278d6f3c19ac1e6131f354ed658ba966f4aba
changhnam <73453695+changhnam@users.noreply.github.com>
2023-03-28 18:47:22 +0900

    Update draw_genomic_distribution_plot.sh

--- 04_genomic_features_of_soL1Rs/draw_genomic_distribution_plot.sh
+++ 04_genomic_features_of_soL1Rs/draw_genomic_distribution_plot.sh
@@ -7,7 +7,7 @@ output_pdf=$2
 
 b37_10mb_bin_bed=data/b37.10mb.5mb.count.bed
 bin_count_bed=$(basename ${input_bed}).count.bed
-DRAW_PLOT=src/draw_genomic_dist_plot.R
+DRAW_PLOT=src/draw_genomic_distribution_plot.R
 fai_path=data/human_g1k_v37.fasta.fai
 
 bedtools intersect -a ${b37_10mb_bin_bed} -b ${input_bed} -c -loj > ${bin_count_bed}
